cbf92305e32815098d2a73458aa764f6ce30bb25
Scarpy019 <Scarpy019@gmail.com>
2023-04-30 15:20:41 +0300

    default 404 page added

--- source/index.ts
+++ source/index.ts
@@ -7,6 +7,8 @@ const app: express.Application = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -16,9 +18,34 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+
+
+app.use('/user', userRouter);
+
+
+// 404 route that accepts all remaining routes
+app.get('*', function(req, res){
+    //set status
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('pages/misc/404', { url: req.url });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.json({url:req.url, error: 'Page not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Page not found');
+});
+
+
 app.listen(3000, async function () {
     await sequelize.sync()
     console.log('App is listening on port 3000!');
 });
-
-app.use('/user', userRouter);
