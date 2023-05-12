82b280a1167ce14e65b102f33b746ab9550a822c
Scarpy019 <Scarpy019@gmail.com>
2023-05-12 13:47:13 +0300

    Listing controller has create page backend

--- source/controllers/ListingController.ts
+++ source/controllers/ListingController.ts
@@ -1,8 +1,9 @@
 import { Listing } from '../models/Listing.js';
 import { Subsection } from '../models/Subsection.js';
+import { User } from '../models/User.js';
 import { Controller } from './BaseController.js';
 
-const listing = new Controller('listing', ['subsectionId']);
+const listing = new Controller('listing', [], ['subsectionId']);
 
 listing.read = async (req, res) => {
 	const subsecId = Number(req.params.subsectionId);
@@ -10,6 +11,17 @@ listing.read = async (req, res) => {
 		const subsection = await Subsection.findByPk(subsecId, { include: [Listing] });
 		if (subsection !== null) {
 			res.render('pages/tt2_listings.ejs', { subsection, subsecId });
+			return;
 		}
 	}
+	// TODO: proper redirect
+	res.sendStatus(404);
 };
+
+listing.interface('create', (req, res) => {
+	if (res.locals.user instanceof User) {
+		res.render('pages/listing/create');
+	} else {
+		res.redirect('/user/login?redirect=' + Buffer.from('/listing/create').toString('base64'));
+	}
+});
