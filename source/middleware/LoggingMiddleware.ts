65103860ffc821131d9056011d36aa90a133bd6a
tower <towergame.epic@gmail.com>
2023-05-18 13:33:40 +0300

    Catch log exception

--- source/middleware/LoggingMiddleware.ts
+++ source/middleware/LoggingMiddleware.ts
@@ -3,20 +3,20 @@ import { UserLog, type Log } from '../models/index.js';
 import { v4 as uuidv4 } from 'uuid';
 import { logger } from '../lib/Logger.js';
 
-export async function logUserAction (req: Request, res: Response, next: NextFunction): Promise<void> {
+export function logUserAction (req: Request, res: Response, next: NextFunction): void {
 	if (res.locals.user !== undefined && res.locals.user !== null) {
 		logger.log(`User ${res.locals.user.username} made ${req.method} to ${req.path}!`);
-		await UserLog.create({
+		UserLog.create({
 			id: uuidv4(),
 			log: { action: req.method, path: req.path } satisfies Log,
 			userId: res.locals.user?.id
-		});
+		}).catch(e => logger.error('Exception occurred writing log to DB: ', e));
 	} else {
 		logger.log(`Anonymous made ${req.method} to ${req.path}!`);
-		await UserLog.create({
+		UserLog.create({
 			id: uuidv4(),
 			log: { action: req.method, path: req.path } satisfies Log
-		});
+		}).catch(e => logger.error('Exception occurred writing log to DB: ', e));
 	}
 	next();
 }
