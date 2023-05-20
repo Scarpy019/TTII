aceae5ad9efe63b23a306c8891382cbd461c6e67
Scarpy019 <Scarpy019@gmail.com>
2023-05-20 19:35:57 +0300

    Added CSRF tokens to headers Support for body CSRF is still supported

--- client/hardening.ts
+++ client/hardening.ts
@@ -39,10 +39,26 @@ export async function fetchWithCSRF (url: string, requestInit: RequestInit): Pro
 	if (Array.isArray(tokenRaw)) token = tokenRaw[tokenRaw.length - 1];
 	else token = tokenRaw;
 	const bodyWithCSRF = { ...body, __CSRFToken: token };
-	const headersWithJSON = { ...(requestInit.headers ?? {}), 'content-type': 'application/json' };
+	const headersWithJSON = { ...(requestInit.headers ?? {}), 'content-type': 'application/json', csrftoken: token };
 	return await fetch(url, {
 		...requestInit,
 		headers: headersWithJSON,
 		body: JSON.stringify(bodyWithCSRF)
+	});
+}
+
+// Custom fetch request that injects the CSRF token into the body
+export async function fetchWithCSRFmultipart (url: string, requestInit: RequestInit): Promise<Response> {
+	const body = requestInit.body ?? null;
+	const tokenRaw: string | string[] = getCookie('CSRFToken');
+	let token = '';
+	if (Array.isArray(tokenRaw)) token = tokenRaw[tokenRaw.length - 1];
+	else token = tokenRaw;
+	const headers = { ...(requestInit.headers ?? {}), 'X-csrf': token };
+	return await fetch(url, {
+		...requestInit,
+		mode: 'cors',
+		headers,
+		body
 	});
 }
