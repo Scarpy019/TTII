fc8299fce5c43a0ea23dd10aa537649d7be32c02
Scarpy019 <Scarpy019@gmail.com>
2023-05-24 00:09:37 +0300

    Reconfigured media to have ordering

--- source/controllers/MediaController.ts
+++ source/controllers/MediaController.ts
@@ -74,27 +74,29 @@ media.create = [
 				callback(null, `${id}.${ext}`);
 			}
 		})
-	}).array('image'),
+	}).array('image[]'),
 	async (req, res) => {
 		const listingId: unknown = req.body.listingId;
+		console.log(req.files, listingId);
 		if (listingId !== undefined && listingId !== null && typeof listingId === 'string') {
 			// check if a user is logged in again for safe typing
 			const user = res.locals.user;
 			if (user !== undefined && user !== null) {
 				const fileData = req.files;
 				if (fileData !== undefined && Array.isArray(fileData)) {
-					fileData.forEach(async file => {
+					fileData.forEach(async (file, i) => {
 						// make the entry
 						const dat = file.filename.split('.');
 						const id = dat[0];
 						const ext = `.${dat[1]}`;
 						await Media.create({
 							uuid: id,
+							orderNumber: i + 1,
 							extension: ext,
 							listingId
 						});
-						res.sendStatus(200);
 					});
+					res.sendStatus(200);
 				} else {
 					res.sendStatus(400);
 				}
@@ -107,3 +109,42 @@ media.create = [
 		}
 	}
 ];
+
+// swap 2 images
+media.update = async (req, res) => {
+	const listingId = req.body.listingId;
+	const swappableAnum = Number(req.body.Anum);
+	const swappableBnum = Number(req.body.Bnum);
+	if (listingId !== undefined && listingId !== null) {
+		const mediaA = await Media.findOne({
+			where: {
+				listingId,
+				orderNumber: swappableAnum
+			}
+		});
+		const mediaB = await Media.findOne({
+			where: {
+				listingId,
+				orderNumber: swappableBnum
+			}
+		});
+		// if only one exists just change the number
+		if (mediaA !== null && mediaB === null) {
+			mediaA.orderNumber = swappableBnum;
+			await mediaA.save();
+		} else if (mediaA === null && mediaB !== null) {
+			mediaB.orderNumber = swappableAnum;
+			await mediaB.save();
+		} else if (mediaA !== null && mediaB !== null) {
+			// if both exist swap image UUIDs
+			const temp = mediaB.uuid;
+			mediaB.uuid = mediaA.uuid;
+			mediaA.uuid = temp;
+			await mediaA.save();
+			await mediaB.save();
+		}
+		// TODO: continue for edit listing
+	} else {
+		res.sendStatus(400);
+	}
+};
