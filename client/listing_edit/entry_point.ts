36dd7e210534a5a93c347b7340342294518de760
Scarpy019 <Scarpy019@gmail.com>
2023-06-16 00:28:06 +0300

    swapping of images in edit page

--- client/listing_edit/entry_point.ts
+++ client/listing_edit/entry_point.ts
@@ -1,2 +1,23 @@
 import './edit.scss';
+import $ from 'jquery';
+import { fetchWithCSRF } from '../hardening.js';
+async function swapImages (a: number, b: number): Promise<void> {
+	const listingId = $('body').data('listing-id');
+	await fetchWithCSRF('/media', {
+		method: 'PUT',
+		body: JSON.stringify({ listingId, Anum: a, Bnum: b })
+	});
+	location.reload();
+}
 
+$('.moveLeft').on('click', (e) => {
+	e.preventDefault();
+	const x: number = Number($(e.currentTarget).data('index'));
+	void swapImages(x, x - 1);
+});
+
+$('.moveRight').on('click', (e) => {
+	e.preventDefault();
+	const x: number = Number($(e.currentTarget).data('index'));
+	void swapImages(x, x + 1);
+});
